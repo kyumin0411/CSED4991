f29ca5e0501d8151cb033099230e497b9a6ce17f
kyumin0411 <rbalsgood@gmail.com>
2023-05-23 09:47:06 +0900

    simplify

--- adversarial_attack.py
+++ adversarial_attack.py
@@ -81,7 +81,7 @@ def DAG_Attack(model: nn.Module,
         targeted: bool = False,
         adv_threshold: float = 0.99,
         max_iter: int = 200,
-        γ: float = 0.5,
+        gamma: float = 0.5,
         p: float = float('inf'),
         callback = None) -> Tensor:
     """DAG attack from https://arxiv.org/abs/1703.08603"""
@@ -97,70 +97,91 @@ def DAG_Attack(model: nn.Module,
     best_adv_percent = torch.zeros(batch_size, device=device)
     adv_found = torch.zeros_like(best_adv_percent, dtype=torch.bool)
     best_adv = inputs.clone()
-    inputs.requires_grad_(True)
+    image = inputs.clone()
+
+    image.requires_grad_(True)
+    
 
     for i in range(max_iter):
         pdb.set_trace()
-        active_inputs = ~adv_found
-        inputs_ = inputs[active_inputs]
-        r_ = r[active_inputs]
-        r_.requires_grad_(True)
-
-        adv_inputs_ = (inputs_ + r_).clamp(0, 1)
-        logits_feature5 = model(adv_inputs_)[5]
+        logits_feature5 = model(image)[5]
         logits = interp(logits_feature5)
+        active_inputs = ~adv_found
+        r_ = r[active_inputs]
 
         adv_log = torch.mul(logits, adv_label)
         clean_log = torch.mul(logits, label)
 
-        adv_direction = adv_log - clean_log
-        pdb.set_trace()
-        # r_.requires_grad_(True)
-        adv_direction_sum = adv_direction.sum()
-        adv_direction_sum.requires_grad_(True)
-        r_m_grad = grad(adv_direction_sum, adv_inputs_, retain_graph=True)[0]
+        r_m = adv_log - clean_log
 
-        if i == 0:
-            num_classes = logits.size(1)
-            if masks is None:
-                masks = labels < num_classes
-            masks_sum = masks.flatten(1).sum(dim=1)
-            masked_labels = labels * masks
-            labels_infhot = torch.zeros_like(logits.detach()).scatter(1, masked_labels.unsqueeze(1), float('inf'))
-
-        dl = multiplier * difference_of_logits(logits, labels=masked_labels[active_inputs],
-                                               labels_infhot=labels_infhot[active_inputs])
-        pixel_is_adv = dl < 0
-        pdb.set_trace()
-        active_masks = masks[active_inputs]
-        adv_percent = (pixel_is_adv & active_masks).flatten(1).sum(dim=1) / masks_sum[active_inputs]
-        is_adv = adv_percent >= adv_threshold
-        adv_found[active_inputs] = is_adv
-        best_adv[active_inputs] = torch.where(batch_view(is_adv), adv_inputs_.detach(), best_adv[active_inputs])
-
-        if callback:
-            callback.accumulate_line('dl', i, dl[active_masks].mean(), title=f'DAG (p={p}, γ={γ}) - DL')
-            callback.accumulate_line(f'L{p}', i, r.flatten(1).norm(p=p, dim=1).mean(), title=f'DAG (p={p}, γ={γ}) - Norm')
-            callback.accumulate_line('adv%', i, adv_percent.mean(), title=f'DAG (p={p}, γ={γ}) - Adv percent')
-
-            if (i + 1) % (max_iter // 20) == 0 or (i + 1) == max_iter:
-                callback.update_lines()
-
-        if is_adv.all():
-            break
-        pdb.set_trace()
-        # r_.requires_grad_(True)
-        r_.requires_grad_(True)
-        loss = (dl[~is_adv] * active_masks[~is_adv]).relu()
-        r_grad = grad(loss.sum(), r_, retain_graph=True)[0]
-        if(r_grad != None):
-            print("r_grad is not None!")
-        else:
-            print("r_grad is None")
+        r_m_sum = r_m.sum()
+        r_m_sum.requires_grad_(True)
+        r_m_grad = grad(r_m_sum, adv_inputs_, retain_graph=True)[0]
+        
         r_grad.div_(batch_view(r_grad.flatten(1).norm(p=p, dim=1).clamp_min_(1e-8)))
-        r_.data.sub_(r_grad, alpha=γ)
+        r_.data.sub_(r_grad, alpha=gamma)
 
-        r[active_inputs] = r_
+        r.data.add_(r_)
+
+        image = (image + r_).clamp(0, 1)
+        
+        # adv_percent = (pixel_is_adv & active_masks).flatten(1).sum(dim=1) / masks_sum[active_inputs]
+        # is_adv = adv_percent >= adv_threshold
+        # adv_found[active_inputs] = is_adv
+
+        # active_inputs = ~adv_found
+        # inputs_ = inputs[active_inputs]
+        # r_.requires_grad_(True)
+
+        # adv_inputs_ = (inputs_ + r_).clamp(0, 1)
+        # logits_feature5 = model(adv_inputs_)[5]
+        # logits = interp(logits_feature5)
+
+
+        # pdb.set_trace()
+        # # r_.requires_grad_(True)
+
+        # if i == 0:
+        #     num_classes = logits.size(1)
+        #     if masks is None:
+        #         masks = labels < num_classes
+        #     masks_sum = masks.flatten(1).sum(dim=1)
+        #     masked_labels = labels * masks
+        #     labels_infhot = torch.zeros_like(logits.detach()).scatter(1, masked_labels.unsqueeze(1), float('inf'))
+
+        # dl = multiplier * difference_of_logits(logits, labels=masked_labels[active_inputs],
+        #                                        labels_infhot=labels_infhot[active_inputs])
+        # pixel_is_adv = dl < 0
+        # pdb.set_trace()
+        # active_masks = masks[active_inputs]
+        # adv_percent = (pixel_is_adv & active_masks).flatten(1).sum(dim=1) / masks_sum[active_inputs]
+        # is_adv = adv_percent >= adv_threshold
+        # adv_found[active_inputs] = is_adv
+        # best_adv[active_inputs] = torch.where(batch_view(is_adv), adv_inputs_.detach(), best_adv[active_inputs])
+
+        # if callback:
+        #     callback.accumulate_line('dl', i, dl[active_masks].mean(), title=f'DAG (p={p}, gamma={gamma}) - DL')
+        #     callback.accumulate_line(f'L{p}', i, r.flatten(1).norm(p=p, dim=1).mean(), title=f'DAG (p={p}, gamma={gamma}) - Norm')
+        #     callback.accumulate_line('adv%', i, adv_percent.mean(), title=f'DAG (p={p}, gamma={gamma}) - Adv percent')
+
+        #     if (i + 1) % (max_iter // 20) == 0 or (i + 1) == max_iter:
+        #         callback.update_lines()
+
+        # if is_adv.all():
+        #     break
+        # pdb.set_trace()
+        # # r_.requires_grad_(True)
+        # r_.requires_grad_(True)
+        # loss = (dl[~is_adv] * active_masks[~is_adv]).relu()
+        # r_grad = grad(loss.sum(), r_, retain_graph=True)[0]
+        # if(r_grad != None):
+        #     print("r_grad is not None!")
+        # else:
+        #     print("r_grad is None")
+        # r_grad.div_(batch_view(r_grad.flatten(1).norm(p=p, dim=1).clamp_min_(1e-8)))
+        # r_.data.sub_(r_grad, alpha=gamma)
+
+        # r[active_inputs] = r_
 
     if callback:
         callback.update_lines()
