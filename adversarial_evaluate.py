fa0c69ec7175d4a3a785b0c7811f27b96ca0fd8f
kyumin0411 <rbalsgood@gmail.com>
2023-05-28 17:42:08 +0900

    test

---
+++ adversarial_evaluate.py
@@ -0,0 +1,85 @@
+import warnings
+from distutils.version import LooseVersion
+from typing import Callable, Dict, Optional, Union
+from configs.test_config_kyumin import get_arguments
+from model.refinenetlw import rf_lw101
+from dataset.adversarial_dataset import adversarialDataSet
+import os.path as osp
+import os
+import numpy as np
+import pdb
+from collections import OrderedDict
+import pickle
+import torch
+from torch import Tensor, nn
+from torch.utils import data
+from tqdm import tqdm
+from torch.autograd import grad, Variable
+
+from util import ConfusionMatrix, make_one_hot, generate_target
+from functools import partial
+import random
+import PIL
+
+IMG_MEAN = np.array((104.00698793, 116.66876762, 122.67891434), dtype=np.float32)
+
+palette = [128, 64, 128, 244, 35, 232, 70, 70, 70, 102, 102, 156, 190, 153, 153, 153, 153, 153, 250, 170, 30,
+           220, 220, 0, 107, 142, 35, 152, 251, 152, 70, 130, 180, 220, 20, 60, 255, 0, 0, 0, 0, 142, 0, 0, 70,
+           0, 60, 100, 0, 80, 100, 0, 0, 230, 119, 11, 32]
+zero_pad = 256 * 3 - len(palette)
+for i in range(zero_pad):
+    palette.append(0)
+
+
+def colorize_mask(mask):
+    new_mask = PIL.Image.fromarray(mask.astype(np.uint8)).convert('P')
+    new_mask.putpalette(palette)
+    return new_mask
+
+
+if __name__ == "__main__":
+    print("start adversarial_evaluate.py")
+
+    args = get_arguments()
+    restore = torch.load(args.restore_from)
+
+    print("restoring loading from previous model is done")
+
+    model = rf_lw101(num_classes=args.num_classes)
+
+    model.load_state_dict(restore['state_dict'])
+    start_iter = 0
+
+    n_classes = args.num_classes
+
+    model.eval()
+
+    device = torch.device("cuda:0")
+
+    model = model.to(device)
+    testloader = data.DataLoader(adversarialDataSet(args.adversarial_data_dir, args.data_city_list, crop_size = (600, 600), mean=IMG_MEAN, scale=False, mirror=False, set=args.set),
+                            batch_size=1, shuffle=False, pin_memory=True)
+
+    interp = nn.Upsample(size=(600, 600), mode='bilinear')
+
+    for index, batch in enumerate(testloader):
+        image, label, size, name = batch
+
+        # label = label.clone().detach().float()
+        # label = label.to(device) 
+        output_feature5 = model(Variable(image).cuda(args.gpu))[5]
+        output = interp(output)
+
+        output = torch.mean(output, dim=0)
+        output = output.cpu().numpy()
+        output = output.transpose(1,2,0)
+        output = np.asarray(np.argmax(output, axis=2), dtype=np.uint8)
+
+        output_col = colorize_mask(output)
+        output = PIL.Image.fromarray(output)
+
+        name = name[0].split('/')[-1]
+        # output.save('%s/%s' % (save_dir_fz, name))
+        color_path = "../data/adversarial/adv_image/" + "Cityscape_colored" + "_" + name[0].split('/')[1]
+        output_col.save(color_path)
+           
---
+++ dataset/adversarial_dataset.py
@@ -0,0 +1,136 @@
+import os
+import os.path as osp
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import collections
+import torch
+import torchvision
+import pdb
+from torch.utils import data
+from PIL import Image
+from torchvision import transforms
+import torchvision.transforms.functional as TF
+
+class adversarialDataSet(data.Dataset):
+
+    def __init__(self, root, list_path, max_iters=None, crop_size=(321, 321), mean=(128, 128, 128), scale=True, mirror=True, ignore_label=255, set='val'):
+        self.root = root
+        self.list_path = list_path
+        self.crop_size = crop_size
+        self.scale = scale
+        self.ignore_label = ignore_label
+        self.mean = mean
+        self.is_mirror = mirror
+        self.img_ids = [i_id.strip() for i_id in open(list_path)]
+        if not max_iters==None:
+            self.img_ids = self.img_ids * int(np.ceil(float(max_iters) / len(self.img_ids)))
+        self.files = []
+        self.set = set
+
+        self.void_classes = [0, 1, 2, 3, 4, 5, 6, 9, 10, 14, 15, 16, 18, 29, 30, -1]
+        self.valid_classes = [
+            7,
+            8,
+            11,
+            12,
+            13,
+            17,
+            19,
+            20,
+            21,
+            22,
+            23,
+            24,
+            25,
+            26,
+            27,
+            28,
+            31,
+            32,
+            33,
+        ]
+        self.ignore_index = 255
+        self.class_map = dict(zip(self.valid_classes, range(19)))
+        for name in self.img_ids:
+            pdb.set_trace()
+            img_file = osp.join(self.root, "adv_image/%s" % ("Cityscape_" + name[:-15]))
+            label_file = osp.join(self.root, "cropped_label/%s" % (self.set, name[:-4]+'.npy'))
+            #pdb.set_trace()
+            self.files.append({
+                "img": img_file,
+                "label": label_file,
+                "name": name
+            })
+        
+
+    def __len__(self):
+        return len(self.files)
+    
+
+    def _apply_transform(self, image, lbl, scale=(0.7, 1.3), crop_size=600):
+        (W, H) = image.size[:2]
+        if isinstance(scale, tuple):
+            scale = random.random() * 0.6 + 0.7
+
+        tsfrms = []
+        tsfrms.append(transforms.Resize((int(H * scale), int(W * scale))))
+        tsfrms = transforms.Compose(tsfrms)
+
+        return tsfrms(image), tsfrms(lbl)
+    
+    def encode_segmap(self, mask):
+        # Put all void classes to zero
+        for _voidc in self.void_classes:
+            mask[mask == _voidc] = self.ignore_index
+        for _validc in self.valid_classes:
+            mask[mask == _validc] = self.class_map[_validc]
+        return mask
+
+    def __getitem__(self, index):
+        datafiles = self.files[index]
+        pdb.set_trace()
+        image = Image.open(datafiles["img"]).convert('RGB')
+        label = Image.open(datafiles["label"])
+        name = datafiles["name"]
+
+        # resize
+        w, h = image.size
+
+        # edit
+        image, label = self._apply_transform(image, label, scale=0.8)
+
+        crop_size = min(600, min(image.size[:2]))
+        i, j, h, w = transforms.RandomCrop.get_params(image, output_size=(crop_size,crop_size)) 
+        image = TF.crop(image, i, j, h, w) 
+        label = TF.crop(label, i, j, h, w)
+
+        if random.random() > 0.5:
+            image = TF.hflip(image)
+            label = TF.hflip(label)
+
+        image = np.asarray(image, np.float32)
+        label = self.encode_segmap(np.array(label, dtype=np.float32))
+
+        lbl = label.astype(float)
+        label = lbl.astype(int)
+        # pdb.set_trace()
+        size = image.shape
+        image = image[:, :, ::-1]  # change to BGR
+        image -= self.mean
+        image = image.transpose((2, 0, 1))
+
+        return image.copy(), label.copy(), np.array(size), name
+
+
+if __name__ == '__main__':
+    dst = GTA5DataSet("./data", is_transform=True)
+    trainloader = data.DataLoader(dst, batch_size=4)
+    for i, data in enumerate(trainloader):
+        imgs, labels = data
+        if i == 0:
+            img = torchvision.utils.make_grid(imgs).numpy()
+            img = np.transpose(img, (1, 2, 0))
+            img = img[:, :, ::-1]
+            plt.imshow(img)
+            plt.show()
